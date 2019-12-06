b4fd7ddf8330cf94f36fe7062eb2882a72675c6d
Casey <casey.t.rock@gmail.com>
2019-12-06 15:50:49 -0500

    need to fix indents but should be pretty close

--- client/main.js
+++ client/main.js
@@ -3,6 +3,42 @@
 //which will allow require() to work in the browser the same way it does in Node :D
 const game = require('./text-rpg-engine');
 
+// Add a room (by default will be beginning room since it was first added)
+const startRoom = game.addRoom('Beginning', 'This is the beginning room');
+// Add a second room (by default will be winning room since it was added last)
+const endRoom = game.addRoom('SecondRoom', 'You did it! You won!');
+// Add required item to room
+endRoom.requirements.push('accessKey');
+
+// Add room prompts
+startRoom.addPrompt(
+    'look',
+    ['look room', 'look at room', 'search room', 'examine room', 'look in'],
+    {
+        'successText': 'You see a room with a door to the right and a statue in the middle.'
+    }
+);
+
+startRoom.addPrompt(
+    // name of prompt (required)
+    'Go right',
+    // keywords that will activate prompt (required)
+    ['go right', 'move right', 'open right', 'enter right', 'door right', 'right door'],
+    // results of prompt
+    {
+        // successful prompt result text (required)
+        'successText': 'You enter in the access code "14052" and successfully open the door.',
+        // failed prompt result text (optional)
+        'failText': 'The door is locked with an access code!',
+        // room to enter as result of prompt (optional)
+        'roomToEnter': 'SecondRoom',
+        // items added to inventory after successful prompt result (optional)
+        'itemsGiven': 'trophy'
+    },
+    // required items to successfully do prompt (optional)
+    ['accessKey']
+);
+
 game.init();
 
 // Send user input to our game (on pressing 'Enter' in the form)
--- player.js
+++ player.js
@@ -17,4 +17,32 @@ export default class Player {
         }
         this.startRoom = startRoom;
     }
+
+    // enter room
+    enterRoom(room) {
+        // assigns the value of room.enter (pass, fail, etc.) to roomResult - that logic is in Room
+        const roomResult = room.enter(this.inventory.items);
+
+        // roomResult[1] is the metAllRequirements bool in room.js; if it's false, well, yeah, it's false
+        if (roomResult[1] === false) {
+            // player didn't have the required items to enter the room
+        } else {
+            // entered room
+            this.currentRoom = room.name;
+        }
+
+        // return results text and whether or not the player successfully entered the room
+        return [roomResult[0], roomResult[1]];
+    }
+
+    // the logic for getItem and dropItem is in inventory.js
+    getItem (item) {
+        this.inventory.addItem(item);
+        return this.inventory;
+    }
+
+    dropItem (item) {
+        this.inventory.dropItem(item);
+        return this.inventory;
+    }
 }
